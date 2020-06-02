729286a5e784ee3aadd04c529901cdbe613e5d82
elifoster <elifosterwy@gmail.com>
2020-06-02 12:44:41 -0700

    :arrow_up: Upgrade to Minecraft 1.13 This unfortunately does not work on 1.14+, so we will need another port. Close #4.

--- src/main/java/io/github/elifoster/dramasplash/DramaSplash.java
+++ src/main/java/io/github/elifoster/dramasplash/DramaSplash.java
@@ -3,11 +3,10 @@ package io.github.elifoster.dramasplash;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.io.BufferedReader;
@@ -15,24 +14,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-@Mod(name = "Drama Generator: Ruby Edition Splash", modid = "drama_splash", version = "2.0.4", acceptedMinecraftVersions = "[1.8,1.12.2]")
+@Mod("drama_splash")
+@Mod.EventBusSubscriber
 public class DramaSplash {
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private boolean isSplashSet = false;
+    private static boolean isSplashSet = false;
 
     @SubscribeEvent
-    public void render(TickEvent.RenderTickEvent event) {
-        Minecraft minecraft = Minecraft.getMinecraft();
+    public static void render(TickEvent.RenderTickEvent event) {
+        Minecraft minecraft = Minecraft.getInstance();
         GuiScreen screen = minecraft.currentScreen;
-        if (event.side.isClient() && screen != null && screen instanceof GuiMainMenu &&
-          !isSplashSet) {
+        if (event.side == LogicalSide.CLIENT && screen instanceof GuiMainMenu && !isSplashSet) {
             GuiMainMenu menu = (GuiMainMenu) screen;
-            ObfuscationReflectionHelper.setPrivateValue(GuiMainMenu.class, menu, getSplash(),
-              "splashText", "field_73975_c", "r");
+            ObfuscationReflectionHelper.setPrivateValue(GuiMainMenu.class, menu, getSplash(), "field_73975_c");
             isSplashSet = true;
         }
     }
@@ -41,7 +34,7 @@ public class DramaSplash {
      * Gets the splash from mc-drama's API
      * @return The random drama, or "Drama Generator fails to generate! [check your logs!]" if there is an IOException.
      */
-    private String getSplash() {
+    private static String getSplash() {
         try {
             URL url = new URL("http://mc-drama.herokuapp.com/raw");
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
